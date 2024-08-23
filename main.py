9ca7a55a69bb31a21ef92f339840492b071ca99e
lovroobreza <lovro.obreza@nudge-labs.com>
2024-08-24 01:01:23 +0200

    random movement

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from turtle import Turtle, Screen
+import random
 
 timmy = Turtle()
 
 timmy.pencolor("red")
 
-t=3
-for _ in range(10):
-    for _ in range(t):
-        timmy.pendown()
-        timmy.right(360 / t)
-        timmy.forward(25 + t * 5)
-        timmy.penup()
-    t = t +1
+moves = [0, 90, 180, 270]
 
+timmy.speed("fastest")
+
+for _ in range(600):
+    timmy.right(random.choice(moves))
+    timmy.forward(20)
 
 screen = Screen()
 screen.exitonclick()
