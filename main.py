213f1758ce55992aade238f3d7f81a18126cc079
lovroobreza <lovro.obreza@nudge-labs.com>
2024-08-24 16:36:09 +0200

    snake game

--- main.py
+++ main.py
@@ -1,41 +1,36 @@
-from turtle import Turtle, Screen
-import random, math
-
+from turtle import Screen
+from snake import Snake
+from fruits import Fruit
 
 screen = Screen()
 screen.setup(width=500, height=500)
+screen.title("Snakez")
+screen.bgcolor("black")
 
-colors=["green", "red", "blue", "yellow", "purple"]
+snake = Snake()
+fruit = Fruit()
 
-offsetY = 500 / 6
 
-turtles=[]
+screen.listen()
+screen.onkey(snake.moveDown, "Down")
+screen.onkey(snake.moveLeft, "Left")
+screen.onkey(snake.moveUp, "Up")
+screen.onkey(snake.moveRight, "Right")
 
-for turtleColor in colors:
-    turt = Turtle(shape="turtle")
-    turt.penup()
-    turt.color(turtleColor)
-    turt.goto(y=-250+offsetY, x=-250 + 10)
-    offsetY = offsetY + 500 / 6
-    turtles.append(turt)
+def game():
+    screen.update()
+    screen.tracer(0)
+    game_over = False
+    while not game_over:
+        snake.moveSnake()
+        screen.update()
+        game_over = snake.detectGameOver()
+        if snake.head.distance(fruit) < 25:
+            snake.enlarge()
+            fruit.refresh()
 
-print(turtles)
-user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race?")
+    
 
-print(user_bet)
+game()
 
-race_over  = False
-winner = ""
-
-while not race_over: 
-    for turtle in turtles:
-        turtle.forward(random.random() * 10)
-        if turtle.xcor() > 200:
-            winner=turtle.color()
-            race_over = True
-
-if winner == user_bet:
-    screen.textinput("you suck", " haha")
-else: 
-    screen.textinput("bravo", "haha")
 screen.exitonclick()
---
+++ snake.py
@@ -0,0 +1,89 @@
+import time
+from turtle import Turtle, Screen
+import os
+
+SPEED = 0.15
+MOVE_DISTANCE=20
+
+HEADING_DOWN=270
+HEADING_UP=90
+HEADING_LEFT=180
+HEADING_RIGHT=0
+
+screen = Screen()
+
+class Snake: 
+
+    def __init__(self):
+        self.snake_parts = []
+        self.createSnake()
+        self.head = self.snake_parts[0]
+
+        
+    def createSnake(self):
+        image = os.path.expanduser("./assets/alja (1).gif")
+        screen.addshape(image)
+        start_x = 0
+        snake_part=Turtle(shape=image)
+        snake_part.shapesize(0.5)
+        snake_part.penup()
+        snake_part.color("blue")
+        snake_part.goto(x=start_x,y=0)
+        start_x = start_x - 20
+        self.snake_parts.append(snake_part)
+
+        for _ in range(2):
+            snake_part=Turtle(shape="square")
+            snake_part.penup()
+            snake_part.color("white")
+            snake_part.goto(x=start_x,y=0)
+            start_x = start_x - 20
+            self.snake_parts.append(snake_part)
+
+    def detectGameOver(self):
+        if (self.snake_parts[0].xcor() > 230): return True
+
+        if (self.snake_parts[0].xcor() < -230): return True
+
+        if (self.snake_parts[0].ycor() > 230): return True
+
+        if (self.snake_parts[0].ycor() < -230): return True
+
+        for part in range(1, len(self.snake_parts)-1, 1):
+            if self.head.distance(self.snake_parts[part]) < 10:
+                return True
+
+        return False
+
+    def moveSnake(self):
+        time.sleep(SPEED)
+
+        for snake_index in range(len(self.snake_parts)-1, 0, -1):
+            next_snake = self.snake_parts[snake_index - 1]
+            self.snake_parts[snake_index].goto(next_snake.xcor(), next_snake.ycor())
+                    
+        self.snake_parts[0].forward(MOVE_DISTANCE)
+
+    def enlarge(self):
+        print("enlarges")
+        snake_part=Turtle(shape="square")
+        snake_part.penup()
+        snake_part.color("white")
+        self.snake_parts.append(snake_part)
+
+
+    def moveUp(self):
+        if self.head.heading() != HEADING_DOWN:
+            self.head.setheading(HEADING_UP)
+
+    def moveDown(self):
+        if self.head.heading() != HEADING_UP:
+            self.head.setheading(HEADING_DOWN)
+
+    def moveLeft(self):
+        if self.head.heading() != HEADING_RIGHT:
+            self.head.setheading(HEADING_LEFT)
+    
+    def moveRight(self):
+        if self.head.heading() != HEADING_LEFT:
+            self.head.setheading(HEADING_RIGHT)
