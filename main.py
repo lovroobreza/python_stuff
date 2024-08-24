c55f4908b8a50e1a4facf0684ac66709ef290521
lovroobreza <lovro.obreza@nudge-labs.com>
2024-08-24 09:56:54 +0200

    randomcolor

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 from turtle import Turtle, Screen
-import random
+import random, math
 
 timmy = Turtle()
+screen = Screen()
 
 timmy.pencolor("red")
 
+
 moves = [0, 90, 180, 270]
 
+screen.colormode(255)
+
+def change_color():
+    R = math.ceil(random.random() * 255)
+    B = math.ceil(random.random()* 255)
+    G = math.ceil(random.random()* 255) 
+
+    print(R,G,B)
+
+    timmy.pencolor(R, G, B)
+
+
 timmy.speed("fastest")
+timmy.pensize(10)
 
 for _ in range(600):
+    change_color()
     timmy.right(random.choice(moves))
     timmy.forward(20)
 
-screen = Screen()
 screen.exitonclick()
