b4c7aa98d4370810c0a0a2dd7342a3bb60c51edf
lovroobreza <lovro.obreza@nudge-labs.com>
2024-08-25 21:35:04 +0200

    tomato - unfinished

--- main.py
+++ main.py
@@ -1,41 +1,14 @@
 from tkinter import *
 
 root = Tk()
-root.title("Lovro")
-root.configure(background="white")
-root.config(padx=20, pady=20)
+root.title("Pomodoro")
+root.config(padx=100, pady=100)
+canvas  = Canvas(width=400, height=444)
+
+tomato_img = PhotoImage(file="./assets/tomato.png")
+canvas.create_image(200, 222, image=tomato_img)
+canvas.pack()
 
 
 
-
-def miles_to_km():
-   miles = float(input.get())
-   km = miles * 1.7
-   km_label.config(text=f"{km}")
-
-input = Entry(width=7)
-
-miles_label = Label(text="Miles")
-
-is_equal_label = Label(text="Is equal")
-
-km_label = Label(text="0")
-
-km_text = Label(text="Kilmoeters")
-
-button = Button(text="Calulcate button", command=miles_to_km)
-
-
-input.grid(column=1, row=0)
-
-miles_label.grid(column=2, row=0)
-
-is_equal_label.grid(column=0, row=1)
-
-km_label.grid(column=1, row=1)
-
-km_text.grid(column=2, row=1)
-
-button.grid(column=1, row=2)
-                   
 root.mainloop()
