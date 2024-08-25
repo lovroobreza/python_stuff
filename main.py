24c49234655b77491f5c7cda282e18189bf8ae5d
lovroobreza <lovro.obreza@nudge-labs.com>
2024-08-25 09:45:39 +0200

    files files

--- main.py
+++ main.py
@@ -1,57 +1,12 @@
-from turtle import Screen
-import time
+import csv
+import pandas
 
-from player import Player
-from car import Car
+#3with open("./files/weather_data.csv") as data_file:
+   # data = csv.reader(data_file)
+#    temps=[]
+ #   for line in data:
+  #      print(line)
 
-screen = Screen()
-screen.setup(width=500, height=700)
-screen.title("crossy roads")
-screen.bgcolor("black")
+data = pandas.read_csv("./files/weather_data.csv")
 
-screen.tracer(0)
-screen.colormode(255)
-player = Player()
-car = Car()
-
-screen.listen()
-screen.onkey(player.moveDown, "Down")
-screen.onkey(player.moveLeft, "Left")
-screen.onkey(player.moveUp, "Up")
-screen.onkey(player.moveRight, "Right")
-
-def playerLost():
-    player_lost = False
-    for c in car.cars:
-        if player.distance(c) < 20:
-            player_lost=True
-            break
-    return player_lost
-
-def game():
-    screen.update()
-    game_over = False
-    player_won = False
-
-    runs = 0
-    
-    while (not game_over) and (not player_won):      
-        runs += 1
-        time.sleep(0.1)
-        if runs % 2 == 0:
-            car.createCar()
-        car.moveCars()
-        screen.update()
-
-        game_over = playerLost()
-        player_won = player.didPlayerWin()
-
-    if player_won:
-        print("gg")
-    if game_over:
-        print("sucks to suck")
-
-
-game()
-
-screen.exitonclick()
+print(data["temp"].max())
