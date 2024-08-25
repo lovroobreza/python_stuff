06a604165b28631fd52b94497c6a14621e577a65
lovroobreza <lovro.obreza@nudge-labs.com>
2024-08-25 15:46:42 +0200

    tkinter miles to kilometers calculator

--- main.py
+++ main.py
@@ -1,12 +1,41 @@
-import pandas
+from tkinter import *
 
-data_set = pandas.read_csv("./files/nato_phonetic_alphabet.csv")
-alpha_dictionary={row.letter:row.code for (index, row) in data_set.iterrows()}
+root = Tk()
+root.title("Lovro")
+root.configure(background="white")
+root.config(padx=20, pady=20)
 
-def sayName(word):
-   for letter in word:
-      print(f"{letter} : {alpha_dictionary[letter.upper()]}\n")
 
-user_word = input("Say your name")
 
-sayName(user_word)
+
+def miles_to_km():
+   miles = float(input.get())
+   km = miles * 1.7
+   km_label.config(text=f"{km}")
+
+input = Entry(width=7)
+
+miles_label = Label(text="Miles")
+
+is_equal_label = Label(text="Is equal")
+
+km_label = Label(text="0")
+
+km_text = Label(text="Kilmoeters")
+
+button = Button(text="Calulcate button", command=miles_to_km)
+
+
+input.grid(column=1, row=0)
+
+miles_label.grid(column=2, row=0)
+
+is_equal_label.grid(column=0, row=1)
+
+km_label.grid(column=1, row=1)
+
+km_text.grid(column=2, row=1)
+
+button.grid(column=1, row=2)
+                   
+root.mainloop()
