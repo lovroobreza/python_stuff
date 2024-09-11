00340043a10db9eb9ac81760c3fed48777da1ba3
lovroobreza <obreza.lovro@gmail.com>
2024-09-11 20:22:07 +0200

    commit

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import random, math
 
 app = Flask(__name__)
 
-jackpot = math.ceil(random.random() * 9)
+@app.route("/")
+def get_home():
+    return render_template('contact.html')
 
-@app.route("/hello")
-def getNumber_2():
-    print('PRINTED')
-    return f"main"
-
-@app.route("/<int:number>")
-def getNumber(number):
-    print(jackpot)
-    if number == jackpot:
-        return f"{number} returned!!"
-    else:
-        return f"sucker"
-
-
+@app.route("/sendemail", methods=["POST"])
+def send_email():
+    print(request.form["name"])
+    return "GG"
 
 app.run(debug=True)
